2eb26695b5e92482f6041822c7d895e75c41c762
Alex Jurkiewicz <alex@jurkiewi.cz>
2020-09-23 15:33:10 +1000

    Don't run init during validate command
    If users want to run init, they can do it explicitly themselves. Running
    init unnecessarily slows down command execution.

--- src/scripts/validate.sh
+++ src/scripts/validate.sh
@@ -3,5 +3,4 @@ if [[ ! -d "$TF_PARAM_PATH" ]]; then
     echo "Path does not exist: \"$TF_PARAM_PATH\""
     exit 1
 fi
-terraform init -input=false -backend=false -no-color "$TF_PARAM_PATH"
 terraform validate -no-color "$TF_PARAM_PATH"
