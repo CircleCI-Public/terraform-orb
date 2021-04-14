5a298753c56d72f3211cde98fe48767ccb66472f
Edd Morgan <eddm@me.com>
2021-04-14 19:45:26 +0100

    Update validate.sh to use -chdir option
    [Terraform 1.5.0](https://github.com/hashicorp/terraform/releases/tag/v0.15.0) removed the ability to specify a working directory for `terraform` commands via positional argument, replacing it with a named `-chdir` argument.

--- src/scripts/validate.sh
+++ src/scripts/validate.sh
@@ -3,5 +3,5 @@ if [[ ! -d "$TF_PARAM_PATH" ]]; then
     echo "Path does not exist: \"$TF_PARAM_PATH\""
     exit 1
 fi
-terraform init -input=false -backend=false -no-color "$TF_PARAM_PATH"
+terraform  -chdir="$TF_PARAM_PATH" init -input=false -backend=false -no-color
 terraform validate -no-color "$TF_PARAM_PATH"
