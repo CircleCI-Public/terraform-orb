14cd55c3e908b0c0812d3f49f31c9cd2d5f5fad9
Kyle T <33272306+KyleTryon@users.noreply.github.com>
2022-05-12 16:06:15 -0400

    fix: add backwards compatible shasum check (#68)
    * fix: add backwards compatible shasum check

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -4,10 +4,25 @@ cd /tmp/terraform-install || return
 wget -P /tmp "https://releases.hashicorp.com/terraform/${TF_PARAM_VERSION}/terraform_${TF_PARAM_VERSION}_${TF_PARAM_OS}_${TF_PARAM_ARCH}.zip"
 wget -nv "https://releases.hashicorp.com/terraform/${TF_PARAM_VERSION}/terraform_${TF_PARAM_VERSION}_SHA256SUMS"
 
+
+# Reported in #61 that recent versions of the Terraform Docker image do not contain shasum
+# This is a backwards compatible fix for verifying the Terraform binary
+checksumCommand() {
+	if [ "$(command -v shasum)" ]; then
+		shasum -a 256 "$1" | cut -d' ' -f1
+	elif [ "$(command -v sha256sum)" ]; then
+		sha256sum "$1" | cut -d' ' -f1
+	else
+		echo "No checksum tools found."
+		exit 1
+	fi
+}
+
 # Validate checksum
 # shellcheck disable=SC2002
 expected_sha=$(cat "terraform_${TF_PARAM_VERSION}_SHA256SUMS" | grep "terraform_${TF_PARAM_VERSION}_${TF_PARAM_OS}_${TF_PARAM_ARCH}.zip" | awk '{print $1}')
-download_sha=$(shasum -a 256 "/tmp/terraform_${TF_PARAM_VERSION}_${TF_PARAM_OS}_${TF_PARAM_ARCH}.zip" | cut -d' ' -f1)
+export -f checksumCommand
+download_sha=$(checksumCommand "/tmp/terraform_${TF_PARAM_VERSION}_${TF_PARAM_OS}_${TF_PARAM_ARCH}.zip")
 echo "Validating download..."
 if [ "$expected_sha" != "$download_sha" ]; then
 	echo "Expected SHA256SUM does not match downloaded file, exiting."
