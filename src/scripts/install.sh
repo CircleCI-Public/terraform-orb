8eb4bd6ed2bc5f933acddb910430d3a68cb95380
Alex Jurkiewicz <alex@jurkiewi.cz>
2022-05-16 13:40:31 +1000

    Refactor install script
    * Use more stringent error handling, so there's no chance errors are
      ignored.
    * Use /tmp/terraform-install for all temporary files, rather tan just
      some.

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,35 +1,54 @@
 #!/bin/bash
-mkdir -p /tmp/terraform-install
-cd /tmp/terraform-install || return
-wget -P /tmp "https://releases.hashicorp.com/terraform/${TF_PARAM_VERSION}/terraform_${TF_PARAM_VERSION}_${TF_PARAM_OS}_${TF_PARAM_ARCH}.zip"
-wget -nv "https://releases.hashicorp.com/terraform/${TF_PARAM_VERSION}/terraform_${TF_PARAM_VERSION}_SHA256SUMS"
 
+set -euo pipefail
+
+download_version() {
+	version=$1
+	local base_url="https://releases.hashicorp.com/terraform/${version}/terraform_${version}"
+	wget -nv "${base_url}_${TF_PARAM_OS}_${TF_PARAM_ARCH}.zip"
+	wget -nv "${base_url}_SHA256SUMS"
+}
+
+init() {
+	mkdir -p /tmp/terraform-install
+	cd /tmp/terraform-install
+}
 
 # Reported in #61 that recent versions of the Terraform Docker image do not contain shasum
 # This is a backwards compatible fix for verifying the Terraform binary
 checksumCommand() {
+	file=$1
 	if [ "$(command -v shasum)" ]; then
-		shasum -a 256 "$1" | cut -d' ' -f1
+		shasum -a 256 "$file" | cut -d' ' -f1
 	elif [ "$(command -v sha256sum)" ]; then
-		sha256sum "$1" | cut -d' ' -f1
+		sha256sum "$file" | cut -d' ' -f1
 	else
 		echo "No checksum tools found."
 		exit 1
 	fi
 }
 
-# Validate checksum
-# shellcheck disable=SC2002
-expected_sha=$(cat "terraform_${TF_PARAM_VERSION}_SHA256SUMS" | grep "terraform_${TF_PARAM_VERSION}_${TF_PARAM_OS}_${TF_PARAM_ARCH}.zip" | awk '{print $1}')
-export -f checksumCommand
-download_sha=$(checksumCommand "/tmp/terraform_${TF_PARAM_VERSION}_${TF_PARAM_OS}_${TF_PARAM_ARCH}.zip")
-echo "Validating download..."
-if [ "$expected_sha" != "$download_sha" ]; then
-	echo "Expected SHA256SUM does not match downloaded file, exiting."
-	exit 1
-fi
+checksum_package() {
+	# Validate checksum
+	# shellcheck disable=SC2002
+	expected_sha=$(cat "terraform_${TF_PARAM_VERSION}_SHA256SUMS" | grep "terraform_${TF_PARAM_VERSION}_${TF_PARAM_OS}_${TF_PARAM_ARCH}.zip" | awk '{print $1}')
+	export -f checksumCommand
+	download_sha=$(checksumCommand "terraform_${TF_PARAM_VERSION}_${TF_PARAM_OS}_${TF_PARAM_ARCH}.zip")
+	echo "Validating download..."
+	if [ "$expected_sha" != "$download_sha" ]; then
+		echo "Expected SHA256SUM does not match downloaded file, exiting."
+		exit 1
+	fi
+}
 
-if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
-unzip -o "/tmp/terraform_${TF_PARAM_VERSION}_${TF_PARAM_OS}_${TF_PARAM_ARCH}.zip" -d /tmp
-$SUDO mv /tmp/terraform /usr/local/bin
+install_terraform() {
+	unzip -o "terraform_${TF_PARAM_VERSION}_${TF_PARAM_OS}_${TF_PARAM_ARCH}.zip"
+	if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
+	$SUDO mv terraform /usr/local/bin
+}
+
+init
+download_version "${TF_PARAM_VERSION}"
+checksum_package
+install_terraform
 terraform version
