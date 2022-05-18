911134d278d200e43afc8df6654457f222d185b4
Alex Jurkiewicz <alex@jurkiewi.cz>
2022-05-19 06:21:44 +1000

    feat: support "latest" Terraform version (#71)
    * Refactor install script
    
    * Use more stringent error handling, so there's no chance errors are
      ignored.
    * Use /tmp/terraform-install for all temporary files, rather tan just
      some.
    
    * Support "latest" and partial match Terraform versions
    
    Instead of requiring users to specify an exact Terraform version, add
    support for three different version specification formats:
    
    * full version (eg 1.2.3): a specific version
    * "latest": latest stable version
    * anything else: the latest version matching this prefix
    
    * Keep the default Terraform at 1.0.0
    
    * fix: remove set -euo pipefail
    
    * fix: shellcheck exit if no dir to cd into
    
    Co-authored-by: KyleTryon <Kyle.Tryon@circleci.com>

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,35 +1,81 @@
 #!/bin/bash
-mkdir -p /tmp/terraform-install
-cd /tmp/terraform-install || return
-wget -P /tmp "https://releases.hashicorp.com/terraform/${TF_PARAM_VERSION}/terraform_${TF_PARAM_VERSION}_${TF_PARAM_OS}_${TF_PARAM_ARCH}.zip"
-wget -nv "https://releases.hashicorp.com/terraform/${TF_PARAM_VERSION}/terraform_${TF_PARAM_VERSION}_SHA256SUMS"
 
+download_version() {
+	version=$1
+	local base_url="https://releases.hashicorp.com/terraform/${version}/terraform_${version}"
+	wget -nv "${base_url}_${TF_PARAM_OS}_${TF_PARAM_ARCH}.zip"
+	wget -nv "${base_url}_SHA256SUMS"
+}
+
+init() {
+	mkdir -p /tmp/terraform-install
+	cd /tmp/terraform-install || exit 1
+}
 
 # Reported in #61 that recent versions of the Terraform Docker image do not contain shasum
 # This is a backwards compatible fix for verifying the Terraform binary
 checksumCommand() {
+	file=$1
 	if [ "$(command -v shasum)" ]; then
-		shasum -a 256 "$1" | cut -d' ' -f1
+		shasum -a 256 "$file" | cut -d' ' -f1
 	elif [ "$(command -v sha256sum)" ]; then
-		sha256sum "$1" | cut -d' ' -f1
+		sha256sum "$file" | cut -d' ' -f1
 	else
 		echo "No checksum tools found."
 		exit 1
 	fi
 }
 
-# Validate checksum
-# shellcheck disable=SC2002
-expected_sha=$(cat "terraform_${TF_PARAM_VERSION}_SHA256SUMS" | grep "terraform_${TF_PARAM_VERSION}_${TF_PARAM_OS}_${TF_PARAM_ARCH}.zip" | awk '{print $1}')
-export -f checksumCommand
-download_sha=$(checksumCommand "/tmp/terraform_${TF_PARAM_VERSION}_${TF_PARAM_OS}_${TF_PARAM_ARCH}.zip")
-echo "Validating download..."
-if [ "$expected_sha" != "$download_sha" ]; then
-	echo "Expected SHA256SUM does not match downloaded file, exiting."
-	exit 1
-fi
+checksum_package() {
+	# Validate checksum
+	# shellcheck disable=SC2002
+	expected_sha=$(cat "terraform_${TF_PARAM_VERSION}_SHA256SUMS" | grep "terraform_${TF_PARAM_VERSION}_${TF_PARAM_OS}_${TF_PARAM_ARCH}.zip" | awk '{print $1}')
+	export -f checksumCommand
+	download_sha=$(checksumCommand "terraform_${TF_PARAM_VERSION}_${TF_PARAM_OS}_${TF_PARAM_ARCH}.zip")
+	echo "Validating download..."
+	if [ "$expected_sha" != "$download_sha" ]; then
+		echo "Expected SHA256SUM does not match downloaded file, exiting."
+		exit 1
+	fi
+}
 
-if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
-unzip -o "/tmp/terraform_${TF_PARAM_VERSION}_${TF_PARAM_OS}_${TF_PARAM_ARCH}.zip" -d /tmp
-$SUDO mv /tmp/terraform /usr/local/bin
+install_terraform() {
+	unzip -o "terraform_${TF_PARAM_VERSION}_${TF_PARAM_OS}_${TF_PARAM_ARCH}.zip"
+	if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
+	$SUDO mv terraform /usr/local/bin
+}
+
+determine_version() {
+	# We might have an exact version, a partial version, or "latest"
+	version_spec=$1
+
+	version_regex="^[0-9]+\.[0-9]+\.[0-9]+$"
+
+	# Exact version needs no further processing
+	if [[ $version_spec =~ $version_regex ]]; then
+		echo "$version_spec"
+		return
+	fi
+
+	index_json=$(curl -sf https://releases.hashicorp.com/terraform/index.json)
+	released_versions=$(echo "$index_json" | jq -r '.versions | keys | .[]' | grep -E "$version_regex" | sort -rV)
+
+	if [[ $version_spec = latest ]]; then
+		head -1 <<< "$released_versions"
+		return
+	fi
+
+	grep -m1 -E "^$version_spec" <<< "$released_versions" || {
+		echo "Couldn't find matching version for '$version_spec'"
+		exit 1
+	}
+	return
+}
+
+init
+tf_version=$(determine_version "$TF_PARAM_VERSION")
+echo "Using Terraform version '$tf_version'"
+download_version "${tf_version}"
+checksum_package
+install_terraform
 terraform version
