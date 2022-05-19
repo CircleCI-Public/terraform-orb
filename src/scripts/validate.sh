db2c078d29de82f72c7cf6875a8eab23fdd6c329
Sam Cook <github@sam.net.nz>
2022-05-20 04:56:33 +1200

    fix: apply custom tf config to validate command (#52)

--- src/scripts/validate.sh
+++ src/scripts/validate.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# Check CLI config file
+if [[ -n "${TF_PARAM_CLI_CONFIG_FILE}" ]]; then
+    if [[ -f "${TF_PARAM_CLI_CONFIG_FILE}" ]]; then
+        export TF_CLI_CONFIG_FILE=${TF_PARAM_CLI_CONFIG_FILE}
+    else
+        echo "Terraform cli config does not exist: ${TF_PARAM_CLI_CONFIG_FILE}"
+        exit 1
+    fi
+fi
 export path=$TF_PARAM_PATH
 if [[ ! -d "$TF_PARAM_PATH" ]]; then
     echo "Path does not exist: $TF_PARAM_PATH"
