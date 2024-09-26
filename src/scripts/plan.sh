ef3478be3aa4b1d7e13c08ed91b067ea3c05b549
Vincent Perez <vincentperez91@gmail.com>
2024-09-26 21:13:48 +0200

    feat: lock param on terraform plan (default to true) (#106)
    * feat: lock param on terraform plan (default to true)
    
    * Update AMI used in tests
    
    * Update tests user data en ami
    
    ---------
    
    Co-authored-by: Mateo Arboleda <mateo.arboleda@circleci.com>

--- src/scripts/plan.sh
+++ src/scripts/plan.sh
@@ -70,4 +70,4 @@ if [[ -n "${TF_PARAM_LOCK_TIMEOUT}" ]]; then
 fi
 export PLAN_ARGS
 # shellcheck disable=SC2086
-terraform -chdir="$module_path" plan -input=false -out=${TF_PARAM_OUT} $PLAN_ARGS
+terraform -chdir="$module_path" plan -input=false -out=${TF_PARAM_OUT} -lock=${TF_PARAM_LOCK} $PLAN_ARGS
