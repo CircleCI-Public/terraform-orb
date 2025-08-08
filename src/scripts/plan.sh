8e929207637b889099d48b62ed80910394c4b496
jj-steno <147073847+jj-steno@users.noreply.github.com>
2025-08-08 15:47:35 -0400

    fix copy/paste error

--- src/scripts/plan.sh
+++ src/scripts/plan.sh
@@ -78,4 +78,4 @@ fi
 export PLAN_ARGS
 # shellcheck disable=SC2086
 
-terraform -chdir="$module_path" plan -auto-approve -parallelism=${TF_PARAM_PARALLELISM} $PLAN_ARGS
+terraform -chdir="$module_path" plan -input=false -parallelism=${TF_PARAM_PARALLELISM} -out=${TF_PARAM_OUT} -lock=${TF_PARAM_LOCK} $PLAN_ARGS
